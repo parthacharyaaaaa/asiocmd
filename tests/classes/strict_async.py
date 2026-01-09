3143247a8c02aff35423f8182d3dc61cf743f8d4
Parth <1008parth@gmail.com>
2026-01-09 22:19:48 +0530

    Adds hook and cmdloop tests to StrictAsyncCmd, closes #3

---
+++ tests/classes/strict_async.py
@@ -0,0 +1,47 @@
+from typing import Any, Literal, Sequence, TextIO
+from acmd import (StrictAsyncCmd,
+                  command, command_helper,
+                  async_command, async_command_helper)
+
+class AsyncHookTestCmd(StrictAsyncCmd):
+    __slots__ = ('output_array')
+
+    def __init__(self, completekey: str = 'tab', prompt: str | None = None, stdin: TextIO | Any | None = None, stdout: TextIO | Any | None = None, use_raw_input: bool = True, intro: str | None = None, ruler: str = "=", doc_header: str = "Documented commands (type help <topic>):", misc_header: str = "Miscellaneous help topics:", undoc_header: str = "Undocumented commands:", excluded_commands: Sequence[str] | None = None):
+        self.output_array: list[str] = [i.__name__ for i in (self.preloop, self.precmd, self.postcmd, self.postloop)]
+        super().__init__(completekey, prompt, stdin, stdout, use_raw_input, intro, ruler, doc_header, misc_header, undoc_header, excluded_commands)
+
+    async def apreloop(self):
+        self.output_array.remove(self.preloop.__name__)
+        self.stdout.write("preloop\n")
+    
+    async def apostloop(self):
+        self.output_array.remove(self.postloop.__name__)
+        self.stdout.write("postloop")
+
+    async def aprecmd(self, line: str):
+        self.output_array.remove(self.precmd.__name__)
+        self.stdout.write("precmd\n")
+        return line
+
+    async def apostcmd(self, stop, line: str):
+        self.output_array.remove(self.postcmd.__name__)
+        self.stdout.write("postcmd\n")
+        return line
+    
+    # Synchronous hook methods derived from BaseCmd, should never be called in StrictAsyncCmd
+    def preloop(self):
+        raise NotImplementedError(f"Synchronous hook method {self.preloop.__name__} called in {StrictAsyncCmd.__name__}")
+    def precmd(self, line: str):
+        raise NotImplementedError(f"Synchronous hook method {self.precmd.__name__} called in {StrictAsyncCmd.__name__}")
+    def postcmd(self, stop, line: str):
+        raise NotImplementedError(f"Synchronous hook method {self.postcmd.__name__} called in {StrictAsyncCmd.__name__}")
+    def postloop(self):
+        raise NotImplementedError(f"Synchronous hook method {self.postloop.__name__} called in {StrictAsyncCmd.__name__}")
+    
+    # NOTE: BaseCmd's method cmdloop when inherited in StrictAsyncCmd already raises NotImplementedError
+    # The reason hook methods are tested explicitly is because they are ideally never called in acmdloop
+
+    @command
+    def foo(self, line: str) -> None: pass
+    @command
+    def exit(self, line: str) -> Literal[True]: return True
---
+++ tests/unit/test_strict_async_cmd.py
@@ -0,0 +1,47 @@
+import pytest
+from tests.conf import test_io
+from tests.classes.strict_async import AsyncHookTestCmd
+from acmd import StrictAsyncCmd
+
+def test_loop(test_io):
+    stdin, stdout = test_io
+    cmd: StrictAsyncCmd = StrictAsyncCmd(stdin=stdin, stdout=stdout, use_raw_input=False)
+
+    failed: bool = False
+    try:
+        cmd.cmdloop()
+    except NotImplementedError:
+        failed = True
+    finally:
+        assert failed, \
+        f"{StrictAsyncCmd.__name__} failed to raise error on calling sync loop {StrictAsyncCmd.cmdloop.__name__}"
+
+@pytest.mark.asyncio
+async def test_base_cmd_hooks(test_io):
+    stdin, stdout = test_io
+    hook_cmd: AsyncHookTestCmd = AsyncHookTestCmd(stdin=stdin, stdout=stdout, use_raw_input=False)
+
+    stdin.write("\n".join(("foo", "exit")))
+    stdin.seek(0)
+
+    await hook_cmd.acmdloop()
+    assert not hook_cmd.output_array, \
+    f"Hook method(s) ({', '.join(str(i) for i in hook_cmd.output_array)}) not invoked"
+
+    output_lines: list[str] = stdout.getvalue().split("\n")
+    # Check order of hook methods, which should be
+    # 1) preloop
+    # 2) Intro
+    # 3) Prompt + precmd
+    # 4) postcmd
+    # 5) postloop
+    
+    assert len(output_lines) == 5, \
+    "Unexpected output captured, does not match expected stdout sequence"
+
+    output_lines[2] = output_lines[2].split()[1]    # Remove prompt from #3 
+    output_lines.pop(1) # Remove captured intro
+    
+    for hook_output in ("preloop", "precmd", "postcmd", "postloop"):
+        assert hook_output == output_lines.pop(0), \
+        "Unexpected output captured, does not match expected stdout sequence"
